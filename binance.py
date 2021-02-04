7b1f24d5f2da4f07564cc024f21a21f3c5ba51bb
efsanex <comert89@gmail.com>
2021-02-04 20:01:06 +0300

    Update binance.py

--- binance.py
+++ binance.py
@@ -2,10 +2,9 @@ from binance.client import Client
 import time
 import requests
 
-binance = Client('api_', 'secret_')
-gt = binance.get_server_time()
-
+binance = Client('+api+', '+secret+')
 tumFiyatlar = binance.get_all_tickers()
+coinler = []
 
 def fiyatBul(sym):
     count = 0
@@ -22,48 +21,95 @@ def hesapla(anlıkFiyat, alımFiyat):
     return degisim
 
 def do():
-    
     coinler = []
     coinGetir = binance.get_account()
-    
     for i in range(0, len(coinGetir['balances'])): 
-        adet = float(coinGetir['balances'][i]['free'])
-        if(adet > 1):
-            coinler.append(coinGetir['balances'][i]['asset']+'USDT')
+        if(float(coinGetir['balances'][i]['locked']) > 0):
+            if(float(coinGetir['balances'][i]['locked']) > 1.0):
+                coinler.append(coinGetir['balances'][i]['asset']+'USDT')
+                if(coinGetir['balances'][i]['asset'] == 'USDT'):
+                    coinler.remove('USDTUSDT')
+                else:
+                    stopLossVar(coinGetir['balances'][i]['asset']+'USDT')
+            
+        if(float(coinGetir['balances'][i]['free']) > 0):
+            if(float(coinGetir['balances'][i]['free']) > 1.0):
+                coinler.append(coinGetir['balances'][i]['asset']+'USDT')
+                if(coinGetir['balances'][i]['asset'] == 'USDT'):
+                    coinler.remove('USDTUSDT')
+                else:
+                    tekLimit(coinGetir['balances'][i]['asset']+'USDT')
+                
 
-    coinler.remove('USDTUSDT')
-
-    for i in range(0, len(coinler)): 
-
-        alınanlar = binance.get_all_orders(symbol=coinler[i], limit=1)
-        adet = float(alınanlar[0]['executedQty'])
-        fiyat = float(alınanlar[0]['cummulativeQuoteQty']) / adet
-
-        coinFiyat = tumFiyatlar[fiyatBul(coinler[i])].get('price')
-
-        fiyatDegisim_ = hesapla(float(coinFiyat),fiyat)
+def stopLossVar(sembol):
+    orderS = binance.get_all_orders(symbol=sembol, limit=2)
+    if(orderS[0]['type'] == 'MARKET'):
+        alınanAdet = float(orderS[0]['executedQty'])
+        toplamFiyat = float(orderS[0]['cummulativeQuoteQty'])
+        almaFiyatı = toplamFiyat / alınanAdet
+        anlıkFiyat = float(tumFiyatlar[fiyatBul(orderS[0]['symbol'])].get('price'))
+        anlıkToplamFiyat = alınanAdet * anlıkFiyat
+        
+        fiyatDegisim_ = hesapla(float(anlıkFiyat),almaFiyatı)
         fiyatDegisim = '% ' +str(float("{:.3f}".format(fiyatDegisim_)))
-
-        almaFiyatı = float(alınanlar[0]['cummulativeQuoteQty'])
-        anlıkFiyat = float(adet * float(coinFiyat))
-
-        sonuc_ = anlıkFiyat-almaFiyatı
+        
+        sonuc_ = toplamFiyat-anlıkToplamFiyat
         sonuc = str(float("{:.3f}".format(sonuc_)))
-
+        
         if(almaFiyatı > anlıkFiyat):
-            sonuc = sonuc + ' dolar zarar'
+            sonuc = ' | '+fiyatDegisim+' | '+ sonuc +' zarar'
+            if(fiyatDegisim_ < -5):
+                sonuc = ''
+                sonuc = ' | '+fiyatDegisim + ' ZARAR | '+ str(float("{:.3f}".format(sonuc_))) +' dikkatli ol :)'
         if(anlıkFiyat > almaFiyatı):
-            sonuc = sonuc + ' dolar kar'
-        if(fiyatDegisim_ < -5):
-            sonuc = 'dikkatli ol :)'
+            sonuc = ' | '+fiyatDegisim+' | '+ sonuc + ' KAR'
 
-        send_text = 'https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+chat_id+'&text='+ coinler[i] +' -> '+ fiyatDegisim + ' -> '+sonuc
+        send_text = 'https://api.telegram.org/bot+botToken+/sendMessage?chat_id='+chatId+'&text='+ orderS[0]['symbol'] + sonuc
         response = requests.get(send_text)
-        print(almaFiyatı)
-        print(anlıkFiyat)
+        print(sonuc)
+        
+    #if(orderS[1]['type'] == 'STOP_LOSS_LIMIT'):
+    #    stoptakiAdet = float(orderS[1]['origQty'])
+    #    almaFiyatı_ = toplamFiyat / stoptakiAdet
+    #    anlıkFiyat_ = tumFiyatlar[fiyatBul(orderS[0]['symbol'])].get('price')
+    #    print(stoptakiAdet)
+    #    print(almaFiyatı_)
+    #    print(anlıkFiyat_)
+
+def tekLimit(sembol):
+    orderS = binance.get_all_orders(symbol=sembol, limit=1)
+    if(orderS[0]['type'] == 'MARKET'):
+        alınanAdet = float(orderS[0]['executedQty'])
+        toplamFiyat = float(orderS[0]['cummulativeQuoteQty'])
+        almaFiyatı = toplamFiyat / alınanAdet
+        anlıkFiyat = float(tumFiyatlar[fiyatBul(orderS[0]['symbol'])].get('price'))
+        anlıkToplamFiyat = alınanAdet * anlıkFiyat
+        
+        fiyatDegisim_ = hesapla(float(anlıkFiyat),almaFiyatı)
+        fiyatDegisim = '% ' +str(float("{:.3f}".format(fiyatDegisim_)))
+        
+        sonuc_ = anlıkToplamFiyat-toplamFiyat
+        sonuc = str(float("{:.3f}".format(sonuc_)))
+        
+        if(almaFiyatı > anlıkFiyat):
+            sonuc = ' | '+fiyatDegisim+' | '+ sonuc +' zarar'
+            if(fiyatDegisim_ < -5):
+                sonuc = ''
+                sonuc = ' | '+fiyatDegisim + ' ZARAR | '+ str(float("{:.3f}".format(sonuc_))) +' dikkatli ol :)'
+           
+        if(anlıkFiyat > almaFiyatı):
+            sonuc = ' | '+fiyatDegisim+' | '+ sonuc + ' KAR'
+            if(fiyatDegisim_ > 9):
+                orderZ = binance.order_market_sell(symbol=orderS[0]['symbol'],quantity=alınanAdet)
+                print(orderZ)
+
+        send_text = 'https://api.telegram.org/bot1682375320:AAFOg9o6D8swAItoevo9b6WuqiQOk5Mt33U/sendMessage?chat_id=461723161&text='+ orderS[0]['symbol'] + sonuc
+        response = requests.get(send_text)
+        print(sonuc)
+        
 
 if __name__ == "__main__":
     while True:
         tumFiyatlar = binance.get_all_tickers()
         do()
-        time.sleep(62)
+        time.sleep(15)
